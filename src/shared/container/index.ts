d72aaa572091a1b70e328e009425689674a9c0c7
Jean <jean.poffo0@gmail.com>
2020-12-15 18:48:45 -0300

    Updated Routes and Container
    - Added Clients and Process Stages.

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -5,7 +5,23 @@ import '@modules/users/providers';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
+import IClientsRepository from '@modules/clients/repositories/IClientsRepository';
+import ClientsRepository from '@modules/clients/infra/typeorm/repositories/ClientsRepository';
+
+import IProcessStagesRepository from '@modules/processStages/repositories/IProcessStagesRepository';
+import ProcessStagesRepository from '@modules/processStages/infra/typeorm/repositories/ProcessStagesRepository';
+
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IClientsRepository>(
+  'ClientsRepository',
+  ClientsRepository,
+);
+
+container.registerSingleton<IProcessStagesRepository>(
+  'ProcessStagesRepository',
+  ProcessStagesRepository,
 );
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
+import clientsRouter from '@modules/clients/infra/http/routes/clients.routes';
+import processStagesRouter from '@modules/processStages/infra/http/routes/processStages.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
+routes.use('/clients', clientsRouter);
+routes.use('/process-stages', processStagesRouter);
 
 export default routes;
